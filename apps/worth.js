d1322c84ac97a07a7cec88dfa17099921bc9540a
prabod <praboderathnayaka@gmail.com>
2016-01-28 18:18:49 +0530

    c7

--- apps/worth.js
+++ apps/worth.js
@@ -1,6 +1,42 @@
 exports.worth = function(req, res) {
   if (req.isAuthenticated()) {
-    res.redirect("/worth/"+req.user.id);
+    var fb = require('fb');
+    function compositeImage(source, watermark, destination, callback) {
+      var exec = require('child_process').exec;
+      var command = [
+        'composite',
+        '-geometry',
+        '+62+68',
+        watermark,
+        source,
+        destination
+      ];
+      exec(command.join(' '), function(err, stdout, stderr) {
+        console.log(stderr);
+        fs.unlink(watermark);
+        callback();
+      });
+
+    }
+    var fs = require('fs'),
+      request = require('request');
+    var download = function(uri, filename, callback) {
+      request.head(uri, function(err, res, body) {
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+      });
+    };
+    var userId = req.user.id;
+    var name = '../data/public/worth/' + Date.now() + '.jpg';
+    var des;
+    download(req.user.photos[0].value, name, function() {
+      console.log('done');
+      des = '../data/public/worth/' + userId + '.png';
+      compositeImage('networrth.png', name, des, function() {
+        req.user.path = "/" + des.substring(15);
+        res.redirect("/worth/"+req.user.id);
+      });
+    });
+
   } else {
     res.render('worthPre', {
       title: "Find Your Net Worth in 10 Years",
@@ -13,49 +49,13 @@ exports.worth = function(req, res) {
 }
 
 exports.display = function(req, res) {
-
-  var fb = require('fb');
-  function compositeImage(source, watermark, destination, callback) {
-    var exec = require('child_process').exec;
-    var command = [
-      'composite',
-      '-geometry',
-      '+62+68',
-      watermark,
-      source,
-      destination
-    ];
-    exec(command.join(' '), function(err, stdout, stderr) {
-      console.log(stderr);
-      fs.unlink(watermark);
-      callback();
-    });
-
-  }
-  var fs = require('fs'),
-    request = require('request');
-  var download = function(uri, filename, callback) {
-    request.head(uri, function(err, res, body) {
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-  };
-  var userId = req.user.id;
-  var name = '../data/public/worth/' + Date.now() + '.jpg';
-  var des;
-  download(req.user.photos[0].value, name, function() {
-    console.log('done');
-    des = '../data/public/worth/' + userId + '.png';
-    compositeImage('networrth.png', name, des, function() {
-      res.render('worthPost', {
-        image: "/" + des.substring(15),
-        title: "Find Your Net Worth in 10 Years",
-        url: "http://fb.00ps.xyz/worth/"+userId,
-        description: "Click Here to Find your Net Worth in 10 Years",
-        IDecription: "Click Here to Find your Net Worth in 10 Years",
-        imageLink: "/" + des.substring(15)
-      });
-    });
-
+  res.render('worthPost', {
+    image: req.user.path,
+    user : req.user.id,
+    title: "Find Your Net Worth in 10 Years",
+    url: "http://fb.00ps.xyz/worth/"+req.user.id,
+    description: "Click Here to Find your Net Worth in 10 Years",
+    IDecription: "Click Here to Find your Net Worth in 10 Years",
+    imageLink: "http://fb.00ps.xyz/worth" + req.user.path
   });
-
 }
